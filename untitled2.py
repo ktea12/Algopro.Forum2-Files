339efefacb8c66f3b589a11d81a8d11c37b85337
Thara <91600181+ktea12@users.noreply.github.com>
2021-11-16 06:58:20 +0700

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,76 @@
+The Expansion of Rome and the Equestrian Order
+
+Used as we are to the terminology and conditions of hereditary monarchy
+and territorial sovereignty, we find it hard to appreciate, or even
+to express in terms of modern politics the difficulties which beset
+the statesmen of Rome at the death of Augustus; and we are further
+tempted to read into the story of that critical period ideas, which
+were only conceivable after the crisis was over; we can hardly avoid
+seeing those days in the light of subsequent events, or speaking
+of them in language which involves anachronism. Our information is
+principally derived from historians, who wrote a century and a half
+after the death of Julius Caesar, when the Government of the Emperor
+and the Senate was established; but the position of the Emperor of
+those days was not the position of Augustus, and the Senate of Trajan
+was not the Senate of Tiberius. The experienced officials who formed
+the majority of the Senate of the Flavian Emperors were no longer the
+hereditary oligarchy by whose capacity Rome had been brought to be
+first among the city states of the world, but which was unequal to the
+task of organizing the Roman empire. The change had, however, escaped
+observation, and the warmest admirers of the Senate of the Republic
+were men whose position had been won for them by the Emperors. Between
+the death of Augustus and the death of Vespasian we have but few
+contemporary historians; we have no letters of Cicero to throw light
+on the inner life of the statesmen of those days; there were private
+records, private letters, and private biographies; we can gather their
+tone from the extracts that have been preserved for us, but we have no
+opportunity of comparing them or checking them. Velleius Paterculus is
+the only contemporary historian of the reign of Tiberius, a portion of
+whose work still exists unabridged; and his narrative stops just at the
+period when we require most light at the conspiracy of Sejanus where
+there is also a gap in the annals of Tacitus. From the books of the
+New Testament we may infer much as to how the Empire appeared at a
+comparatively early period to the inhabitants of Greater Rome, much
+also from Josephus, a little from Philo, but we cannot people the
+Rome of Tiberius, as we can people the Rome of Augustus and the Rome
+of Cicero. Two facts stand clear to us from the pages of Tacitus, and
+in a less degree from those of Suetonius, that the Imperial Family was
+divided, that the old Roman princely houses never forgave the Empire,
+and that there was a Republican reaction in opinion at the centre of
+the Empire. History has repeated itself; just as the Curia of today
+cannot forgive the monarchy which represents the unity of Italy, so
+the Curia of the first century of the Christian era was irreconcilable
+to the monarchical constitution which represented the unity of the
+Empire. The Roman princes who wrote the memoirs of their houses for
+the edification of their children, and the delectation of their
+friends never inquired into the authority of a story derogatory to
+the Emperors, and the one Emperor, who was never spared was Tiberius;
+it is no exaggeration to say that the madness of Caligula, and the
+monstrous freaks of Nero are dealt with tenderly by the writers of the
+silver age, if we compare the accounts of these with the deliberate
+malignity which attends on every word and action of Tiberius; and yet
+common sense tells us that only a very able man could have succeeded
+Augustus without breaking up his work. At the death of Augustus it was
+still possible that there would be no second Emperor; at the death of
+Tiberius the Roman Emperor had become an institution, the pivot upon
+which the whole machinery of civilized existence turned throughout
+the world. Hence the peculiar bitterness against Tiberius; the Curia
+felt that in his reign their last chance had gone, and more than this,
+that he had been in some sense a traitor to his own caste. Neither the
+Julian nor the Octavian families had been among the foremost houses
+of Rome, till the genius of the first Caesar raised them from their
+comparative obscurity; but many of the most important events in the
+history of Rome, no less than her buildings, her roads, her aqueducts,
+and many of her public monuments, were associated with the Claudian
+stock, and the Livian, with which it was inter married, was only less
+distinguished. Augustus had been tolerated, for his services to the
+State could not be disregarded, but some day Augustus would die; he did
+die; his power fell into the hands of the most prominent representative
+of the old Roman nobility; the opportunity for a restoration of the
+narrow oligarchy of the Republic came, and it passed away for ever. Two
+years after the death of Tiberius his lunatic successor was stabbed by
+a soldier whom he had insulted; the State was left a few days without a
+head, and the Curia was so inanimate that it could neither restore its
+own rule, nor provide a new Emperor; it had to accept apparently at the
+dictation of the soldiers in the Praetorian barracks a man of letters
+who had hitherto been the laughing stock of the Imperial family.
